dec571be0235b6d48329e76ecb443e15402639d2
Ryan Sheppard <ryandsheppard95@gmail.com>
2026-04-10 21:08:31 -0400

    remove static mount

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from app.routes import router
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(router)
 
 templates = Jinja2Templates(directory="templates")
